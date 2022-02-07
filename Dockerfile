c6d9f010f19ccf96a4e0ce5e0824c7e96be7e69a
Niklas Reimer <n.reimer@uni-luebeck.de>
2022-02-08 00:54:25 +0100

    add docker

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM maven:3.8.4-openjdk-11-slim as build
+
+COPY $PWD /mpiproxy
+WORKDIR /mpiproxy
+
+RUN mvn install
+
+FROM gcr.io/distroless/java11-debian11
+COPY --from=build /mpiproxy/target/mpiproxy-*.jar /app/mpiproxy.jar
+CMD ["/app/mpiproxy.jar"]
--- src/main/java/de/uksh/medic/mpiproxy/MpiProxy.java
+++ src/main/java/de/uksh/medic/mpiproxy/MpiProxy.java
@@ -1,11 +1,13 @@
 package de.uksh.medic.mpiproxy;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import de.uksh.medic.mpiproxy.settings.ConfigurationLoader;
 import de.uksh.medic.mpiproxy.settings.Settings;
@@ -13,15 +15,17 @@ import de.uksh.medic.mpiproxy.settings.Settings;
 @SpringBootApplication
 public class MpiProxy {
 
-	public static void main(String[] args) throws FileNotFoundException {
-        InputStream settingsYaml = ClassLoader.getSystemClassLoader().getResourceAsStream("settings.yaml");
+	public static void main(String[] args) throws IOException {
+		Resource resource = new ClassPathResource("settings.yaml");
+        InputStream settingsYaml = resource.getInputStream();
         if (args.length == 1) {
+			settingsYaml.close();
             settingsYaml = new FileInputStream(args[0]);
         }
 
         ConfigurationLoader configLoader = new ConfigurationLoader();
         configLoader.loadConfiguration(settingsYaml, Settings.class);
-
+		settingsYaml.close();
 		SpringApplication.run(MpiProxy.class, args);
 	}
 
